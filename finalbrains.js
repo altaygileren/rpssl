386007d87bc23e46a6dd2f92066e95c194dc0ea2
erenaltaygil <altaygileren@gmail.com>
2018-07-08 21:13:13 -0400

    progress made on connecting JS to buttons and almost done with design of page

--- finalbrains.js
+++ finalbrains.js
@@ -1,64 +1,61 @@
-const choices = [
-{
-	name: "rock",
-	strength: ["scissor", "lizard"],
-	weakness: ["paper", "spock"]
-},
-{
-	name: "paper",
-	strength: ["rock", "spock"],
-	weakness: ["scissor", "lizard"]
-},
-{
-	name: "scissor",
-	strength: ["paper", "lizard"],
-	weakness: ["rock", "spock"]
-},
-{
-	name: "lizard",
-	strength: ["spock", "paper"],
-	weakness: ["scissor", "rock"]
-},
-{
-	name: "spock",
-	strength: ["rock", "scissor"],
-	weakness: ["lizard", "paper"]
-}
-];
-
-
-
-let picked;
-function userPicker() {
-	let el = document.querySelectorAll('.clickedPick');
-	console.log(el);
-	for(var i=0; i < el.length; i++){
-		el[i].addEventListener('click', function () {
-			// alert('hello ' + this.id);
-			picked = this.id;
-		}, false);
-	}
-	return picked;
-}
-
-
-function practice() {
-if (userPicker() === "rock") {
-	alert("yes");
-} else {
-	alert('no');
-}
-}
-
-// function userSelection() {
-// 	computerChoice = choices[Math.floor(Math.random() * choices.length)];
-// 	if (computerChoice.weakness.includes(userPicker())){
-// 		usertracker++;
-// 		alert("User wins");
-// 	} else if (computerChoice.name === pick) {
-// 	} else {
-// 		comptracker++;
-// 	}
-// 	let userPoints = document.querySelector('#userScore').innerHTML = usertracker;
-// 	let computerPoints = document.querySelector('#computerScore').innerHTML = comptracker;
+// const choices = [
+// {
+// 	name: "rock",
+// 	strength: ["scissor", "lizard"],
+// 	weakness: ["paper", "spock"]
+// },
+// {
+// 	name: "paper",
+// 	strength: ["rock", "spock"],
+// 	weakness: ["scissor", "lizard"]
+// },
+// {
+// 	name: "scissor",
+// 	strength: ["paper", "lizard"],
+// 	weakness: ["rock", "spock"]
+// },
+// {
+// 	name: "lizard",
+// 	strength: ["spock", "paper"],
+// 	weakness: ["scissor", "rock"]
+// },
+// {
+// 	name: "spock",
+// 	strength: ["rock", "scissor"],
+// 	weakness: ["lizard", "paper"]
 // }
+// ];
+
+
+
+let userPoints = 0;
+let compPoints = 0;
+let userScore = document.getElementById('userScore');
+let compScore = document.getElementById('compScore');
+let scoreBoard = document.querySelector('.scoreBoard');
+let rockChoice = document.getElementById('rock');
+let paperChoice = document.getElementById('paper');
+let scissorChoice = document.getElementById('scissors');
+let spockChoice = document.getElementById('spock');
+let lizardChoice = document.getElementById('lizard');
+
+
+main userClick() {
+	rockChoice.addEventListener('click', function() {
+		alert("hello");
+	});
+	paperChoice.addEventListener('click', function() {
+		alert("hello");
+	});
+	scissorChoice.addEventListener('click', function() {
+		alert("hello");
+	});
+	spockChoice.addEventListener('click', function() {
+		alert("hello");
+	});
+	lizardChoice.addEventListener('click', function() {
+		alert("hello");
+	});
+}''
+
+
