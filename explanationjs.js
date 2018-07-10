371d38647dc8c29efe0c05e9cdd6fa530e7abc59
erenaltaygil <altaygileren@gmail.com>
2018-07-09 23:24:13 -0400

    project finished

---
+++ explanationjs.js
@@ -0,0 +1,6 @@
+let modalContent = document.querySelector('.compModal');
+let userClickPlay = document.querySelector('#play');
+
+userClickPlay.addEventListener('click', function() {
+  modalContent.style.display = 'block';
+})
